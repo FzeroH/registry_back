a94f86a91948d8861f187c5af644c9dfda8602bc
FzeroH <vampir666x@gmail.com>
2024-06-07 13:53:55 +0300

    Добавил проверку доступа пользователя к функционалу системы

---
+++ middleware/checkUser.js
@@ -0,0 +1,42 @@
+const { db } = require('../config/pg.config');
+
+module.exports.checkAdmin = async function (req, res, next) {
+    try {
+        const { user_id, role_id } = req.headers
+        const response = await db.oneOrNone(`select user_id, role_id from "user" where user_id = ${user_id}`);
+        if (role_id == response.role_id) {
+            if (response.role_id === 1){
+                next()
+            } else {
+                res.status(403).json({ error: "У вас нет доступа к данной функции!" })
+            }
+        } else {
+            res.status(403).json({ error: "У вас нет доступа к данной функции!" })
+        }
+    }
+    catch (e) {
+        console.log(e)
+        res.status(401).json({ message: 'Для совершения действия необходимо авторизоваться' })
+    }
+}
+
+module.exports.checkEmployee = async function (req, res, next) {
+    try {
+        const { user_id, role_id } = req.headers
+        const response = await db.oneOrNone(`select user_id, role_id from "user" where user_id = ${user_id}`);
+        if (role_id == response.role_id) {
+            if (response.role_id === 1 || response.role_id === 2){
+                next()
+            }
+            else {
+                res.status(403).json({ error: "У вас нет доступа к данной функции!" })
+            }
+        } else {
+            res.status(403).json({ error: "У вас нет доступа к данной функции!" })
+        }
+    } 
+    catch (e) {
+        console.log(e)
+        res.status(401).json({ message: 'Для совершения действия необходимо авторизоваться' })
+    }
+}
