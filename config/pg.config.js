8bc2242e524971ff19c90f799783edf049eef3ae
FzeroH <vampir666x@gmail.com>
2024-06-13 22:42:42 +0300

    .

--- config/pg.config.js
+++ config/pg.config.js
@@ -1,5 +1,5 @@
 const pgp = require("pg-promise")();
 
-const db = pgp(`postgres://postgres:QwasSawq1@127.0.0.1:5432/equipments`);
+const db = pgp(`postgres://postgres:Ptvyfz-Ndthlm.@45.140.19.41:5432/equipments`);
 
 module.exports = { db }
